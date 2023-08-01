4bca52b94ff9cfbdfbfcee9fa356130eeac2c12f
MiraKarate <mira.herrarte@gmail.com>
2023-08-01 10:40:33 +0200

    added space in Eventlist short info

--- src/components/Event.js
+++ src/components/Event.js
@@ -15,7 +15,7 @@ const Event = ({ event }) => { // Destructure the 'event' prop correctly
     <div className='event' id='event'>
       <h2 className='title' style={{ fontWeight: 'bold' }}>Title: {event.summary}</h2>
       <p className='shortInfo'> {event.start.dateTime} ({event.start.timeZone})</p>
-      <p className='shortInfo'> @{event.summary}|{event.location}</p>
+      <p className='shortInfo'> @{event.summary}| {event.location}</p>
       <button className='detailsButton'onClick={toggleCollapse}> {collapsed ?'Show details': 'Hide details'} </button>
       {!collapsed && (
         <div className = "details">
