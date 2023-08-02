43d390bcbd934add91811fd7f10d203e02ed7264
MiraKarate <mira.herrarte@gmail.com>
2023-08-02 13:17:15 +0200

    added spacing in event.js between date and time

--- src/components/Event.js
+++ src/components/Event.js
@@ -14,7 +14,7 @@ const Event = ({ event }) => { // Destructure the 'event' prop correctly
   return (
     <div className='event' id='event'>
       <h2 className='title' style={{ fontWeight: 'bold' }}>Title: {event.summary}</h2>
-      <p className='shortInfo'> {event.start.dateTime} ({event.start.timeZone})</p>
+      <p className='shortInfo'> {event.start.dateTime} | ({event.start.timeZone})</p>
       <p className='shortInfo'> @{event.summary} | {event.location}</p>
       <button className='detailsButton'onClick={toggleCollapse}> {collapsed ?'Show details': 'Hide details'} </button>
       {!collapsed && (
